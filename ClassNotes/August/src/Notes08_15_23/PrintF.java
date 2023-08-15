c96a84cd9053028cd344bbfdf070f2f539f16a94
Tanner Lane <tannerln7@gmail.com>
2023-08-15 11:05:53 -0400

    08/15/2023

---
+++ ClassNotes/August/src/Notes08_15_23/PrintF.java
@@ -0,0 +1,10 @@
+package Notes08_15_23;
+
+public class PrintF {
+    public static void main(String[] args) {
+        String item = "Laptop";
+        double price = 299.856;
+        int warranty = 2;
+        System.out.printf("Item : %10s\nPrice : %10s\nWarranty : %10s\n", item, price, warranty);
+    }
+}
--- ClassNotes/August/src/notes_8_25_23.java
+++
@@ -1,6 +0,0 @@
-public class notes_8_25_23 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        // These are some notes... There wasn't much to take notes on tbh...
-    }
-}
