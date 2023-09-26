3905660acdc42d5f95b63248922536dcf3b20dd6
Tanner Lane <tannerln7@gmail.com>
2023-09-26 11:17:56 -0400

    09/26/23

---
+++ ClassNotes/August/src/Notes09_26_23/InputFileDemo.java
@@ -0,0 +1,72 @@
+package Notes09_26_23;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class InputFileDemo {
+    public static void main(String[] args) {
+        Scanner input = initializeScanner("input.txt", "C:\\Users\\tanne\\Desktop\\CISP1020\\CISP1020\\ClassNotes\\August\\src\\Notes09_26_23\\input.txt");
+        PrintWriter out = initializePrintWriter("output.txt", "C:\\Users\\tanne\\Desktop\\CISP1020\\CISP1020\\ClassNotes\\August\\src\\Notes09_26_23\\output.txt");
+
+        double sum = 0;
+        while (input != null && input.hasNextDouble()) {
+            double number = input.nextDouble();
+            sum += number;
+            System.out.println(number);
+            if (out != null) {
+                out.println(number);
+            }
+        }
+        System.out.println("Sum = " + sum);
+        if (out != null) {
+            out.println("Sum = " + sum);
+        }
+        if (out != null) {
+            out.close();
+        }
+    }
+
+    private static Scanner initializeScanner(String filePath, String fullPath) {
+        Scanner scanner = null;
+        try {
+            File file = new File(filePath);
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found. Creating...");
+            createFile(fullPath);
+            File file = new File(fullPath);
+            try {
+                scanner = new Scanner(file);
+            } catch (FileNotFoundException ex) {
+                System.out.println("File creation failed.");
+            }
+        }
+        return scanner;
+    }
+
+    private static PrintWriter initializePrintWriter(String filePath, String fullPath) {
+        PrintWriter writer = null;
+        File file = createFile(fullPath);
+        assert file != null;
+        try {
+            writer = new PrintWriter(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("File creation failed.");
+        }
+        return writer;
+    }
+
+    private static File createFile(String fullPath) {
+        try {
+            File file = new File(fullPath);
+            if (file.createNewFile()) {
+                System.out.println("File created.");
+            }return file;
+        } catch (IOException e) {
+            System.out.println("File creation failed.");
+        }return null;
+    }
+}
