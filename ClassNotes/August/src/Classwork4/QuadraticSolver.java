a4e689bdf3f7e07ace9e6669f6aea8ff589d36bc
Tanner Lane <tannerln7@gmail.com>
2023-10-17 11:56:05 -0400

    discriminant to determinant

--- ClassNotes/August/src/Classwork4/QuadraticSolver.java
+++ ClassNotes/August/src/Classwork4/QuadraticSolver.java
@@ -3,10 +3,10 @@ public class QuadraticSolver {
 
     public static double[] solveQuadraticEQ(double a, double b, double c) throws NanException {
         // Calculate the discriminant
-        double discriminant = b * b - 4 * a * c;
+        double determinant = b * b - 4 * a * c;
 
         // Check if the discriminant is negative (no real solutions)
-        if (discriminant < 0) {
+        if (determinant < 0) {
             throw new NanException("No square root for negative numbers; make sure that b^2 is greater than 4*a*c.");
         }
 
@@ -19,8 +19,8 @@ public class QuadraticSolver {
         double root1;
         double root2;
 
-        root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-        root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        root1 = (-b + Math.sqrt(determinant)) / (2 * a);
+        root2 = (-b - Math.sqrt(determinant)) / (2 * a);
 
         // Create an array to store the solutions
         return new double[]{root1, root2};
